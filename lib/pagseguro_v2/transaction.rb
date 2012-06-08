45f6f652e775c2062bb3cd859a5f41578b531a15
Cirdes Henrique <cirdes@codemedia.com.br>
2012-06-08 13:01:25 -0300

    transaction working with nested hashes as hash

--- lib/pagseguro_v2/transaction.rb
+++ lib/pagseguro_v2/transaction.rb
@@ -1,13 +1,13 @@
 module PagseguroV2
   class Transaction  < Hashie::Trash
-    property :status
-    property :type
-    property :reference
-    property :code
     property :date
-    property :payment_method
+    property :code
+    property :reference
+    property :type
+    property :status
 
-    property :last_eventDate, :from => :lastEventDate
+    property :payment_method, :from => :paymentMethod
+
     property :gross_amount, :from => :grossAmount
     property :discount_amount, :from => :discountAmount
     property :fee_amount, :from => :feeAmount
@@ -19,29 +19,5 @@ module PagseguroV2
     property :items
     property :sender
     property :shipping
-
-    def initialize(options)
-      self.payment_method = PaymentMethod.new(options[:paymentMethod]) if options[:paymentMethod]
-      #self.sender = Sender.new(options[:sender]) if options[:sender]
-      self.shipping = Shipping.new(options[:shipping]) if options[:shipping]
-      self.items = options[:items] if options[:items]
-      options.delete(:payment_method)
-     # options.delete(:sender)
-      options.delete(:shipping)
-      options.delete(:items)
-      super(options)
-    end
-
-    def sender=(sender)
-      self[:sender] = Sender.new(sender) if sender.is_a? Hash
-      self[:sender] = sender if sender.is_a? Sender
-    end
-
-    def items=(items)
-      items = items.map{ |i| Item.new(i) } if items.is_a? Array
-      items = [Item.new(items)] if items.is_a? Hash
-      self[:items] = items
-    end
-
   end
 end
